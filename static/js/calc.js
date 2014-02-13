cd5c5ee933bb846e3135c3089f7f13dae66560a7
Ronak Ahir <ronak.ahir@berkeley.edu>
2014-02-12 20:00:55 -0800

    Calculator Fixes

--- static/js/calc.js
+++ static/js/calc.js
@@ -24,6 +24,7 @@ $(function() {
     var outputText = $('#output').text() + '2';
     if (outputText === '02') {
       outputText = '2';
+    }
     $('#output').text('outputText');
   });
 
@@ -31,6 +32,7 @@ $(function() {
     var outputText = $('#output').text() + '3';
     if (outputText === '03') {
       outputText = '3';
+    }
     $('#output').text('outputText');
   });
 
@@ -38,6 +40,7 @@ $(function() {
     var outputText = $('#output').text() + '4';
     if (outputText === '04') {
       outputText = '4';
+    }
     $('#output').text('outputText');
   });
 
@@ -45,6 +48,7 @@ $(function() {
     var outputText = $('#output').text() + '5';
     if (outputText === '05') {
       outputText = '5';
+    }
     $('#output').text('outputText');
   });
 
@@ -52,6 +56,7 @@ $(function() {
     var outputText = $('#output').text() + '6';
     if (outputText === '06') {
       outputText = '6';
+    }
     $('#output').text('outputText');
   });
 
@@ -59,6 +64,7 @@ $(function() {
     var outputText = $('#output').text() + '7';
     if (outputText === '07') {
       outputText = '7';
+    }
     $('#output').text('outputText');
   });
 
@@ -66,6 +72,7 @@ $(function() {
     var outputText = $('#output').text() + '8';
     if (outputText === '08') {
       outputText = '8';
+    }
     $('#output').text('outputText');
   });
 
@@ -73,6 +80,7 @@ $(function() {
     var outputText = $('#output').text() + '9';
     if (outputText === '09') {
       outputText = '9';
+    }
     $('#output').text('outputText');
   });
 
