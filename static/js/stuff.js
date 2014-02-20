d74053e1330cd770e229bbbe651b87ba5e1637ae
Ronak Ahir <ronak.ahir@berkeley.edu>
2014-02-19 19:58:35 -0800

    Completed Stuff Page

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -1,24 +1,31 @@
 alert('Welcome to my stuff page!'); // edit me!
 
 // Problem 1 (Say Hello!) ---------------------------------------------------
-$('#say_hello').click(function('Hello world!') {
-  // WRITE CODE HERE
+$('#say_hello').click(function() {
+  alert('Hello world!')
 });
 
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
-  //WRITE CODE HERE
+   $('#houdini_text').hide();
 });
 
 $('#reappear').click(function() {
-  //WRITE CODE HERE
+   $('#houdini_text').show();
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
-// WRITE CODE HERE
+$('#pink').click(function() {
+   $('#tickled_text').css('color','pink');
+});
 
 
 // Problem 4 (Greet Me) -----------------------------------------------------
-// WRITE CODE HERE
+$('#greet').click(function() {
+   $('#my_name').val();
+   alert('Hello ' + $('#my_name').val())
+});
+
+
