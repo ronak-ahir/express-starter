48915e804e1a3fcbca73ec70e0e81c39ae4bc0a9
Ronak Ahir <ronak.ahir@berkeley.edu>
2014-03-12 19:19:04 -0700

    Mastered circles and squares!

---
+++ static/js/funcs.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  //this is how we acquire control of the canvas
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext("2d");
+
+  $('#clear').click(function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  var drawSquare = function(x, y, sideLen, color) {
+    context.strokeStyle = color; 
+    context.strokeRect(x,y,sideLen,sideLen); 
+  };
+
+  var drawCircle = function(x, y, radius, color) {
+    context.strokeStyle = color;
+    context.beginPath();
+    context.arc(x,y,radius,0,2 * Math.PI); 
+    context.closePath();
+    context.stroke();
+  };
+
+  // Write drawTriplet function here
+
+  // Challenge:
+  // Write drawTriangle, drawTriforce, drawTripleTriforce,
+  // drawSierpinski functions here
+
+  $('#p1').click(function() {
+    drawSquare(100, 200, 50, 'blue');
+  });
+
+  $('#p2').click(function() {
+    drawSquare(300, 100, 25, 'green');
+  });
+
+  $('#p3').click(function() {
+    drawCircle(100, 200, 50, 'red');
+  });
+
+  $('#p4').click(function() {
+    drawCircle(300, 100, 25, 'black');
+  });
+
+ $('#p5').click(function() {
+    drawSquare(100, 100, 50, 'red');
+    drawCircle(125, 125, 25, 'blue');
+    drawCircle(125, 150, 25, 'green');
+    drawCircle(125, 100, 25, 'green');
+    drawCircle(150, 125, 25, 'green');
+    drawCircle(100, 125, 25, 'green');
+  });
+
+  //---------------------------------------------------------------------------
+  //Write your code for p5-p11 here
+  //
+
+});
